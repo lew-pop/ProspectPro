a20793bc3d0cc2b73d0d792f36d631a9f4ed2c2b
Lewis Quick <lew.pop.quick@gmail.com>
2025-03-09 13:53:57 -0700

    Fixed app.py

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ server = app.server
 
 # Run the app
 if __name__ == "__main__":
-    # app.run_server(host="0.0.0.0", port=8050)
-    app.run_server(debug=True, port=8050)
+    app.run_server(host="0.0.0.0", port=8050)
+    #app.run_server(debug=True, port=8050)
